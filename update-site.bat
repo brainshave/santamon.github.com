3700309d1e48b678c49b574b6717fe843f384342
Szymon Witamborski <santamon@gmail.com>
2011-09-11 19:51:22 +0200

    Added batch file to generate site.

---
+++ update-site.bat
@@ -0,0 +1,11 @@
+call jekyll
+
+@echo on
+
+xcopy _site\* ..\longstandingbug-target /S /Y /Q
+
+cd ..\longstandingbug-target
+call git add .
+call git commit -m "Site update from src branch."
+
+rem cd ..\longstandingbug
